1248f841e39f91a811bad3d265826d6cc4c2909e
sarfraz <eng.sarfraz@gmail.com>
2019-03-12 12:28:16 +0500

    first commit of websearchapp development using Flask

---
+++ MyModules/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+name='DBcm',
+version='1.0',
+description='The Head First Python Search Tools',
+author='Sarfraz',
+author_email='eng.sarfraze@gmail.com',
+url='sarfraz.com',
+py_modules=['DBcm'],
+)
